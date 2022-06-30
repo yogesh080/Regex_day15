0be480200ea465126030ff29e323d9dbfddf502e
Yogesh <yogeshbietjhs@gmail.com>
2022-06-30 17:35:32 +0530

    inicial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace UserRegistration_Day19
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserRegistration userRegistration = new UserRegistration();
+            Console.Write("Enter the First name: ");
+            string Firstname = Console.ReadLine();
+
+            if (userRegistration.validateFirstName(Firstname))
+                Console.WriteLine("True");
+            else
+                Console.WriteLine("False");
+        }
+    }
+}
---
+++ UserRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration_Day19
+{
+    class UserRegistration
+    {
+        
+
+        public string regex_for_name = "^[A-Z]{1}[a-z]{2,10}$";
+
+        public bool validateFirstName(string fName)
+        {
+
+            return Regex.IsMatch(fName, regex_for_name);
+
+        }
+    }
+}
