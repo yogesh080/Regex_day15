01ae7355d055d09d6b47099b43f4b3183bbf8c5c
Yogesh <yogeshbietjhs@gmail.com>
2022-06-30 20:28:35 +0530

    incicial commit

--- Program.cs
+++ Program.cs
@@ -5,13 +5,20 @@
         public static void Main(string[] args)
         {
             UserRegistration userRegistration = new UserRegistration();
+
+
             Console.Write("Enter the First name: ");
             string Firstname = Console.ReadLine();
+            userRegistration.validatefirstname(Firstname);
 
-            if (userRegistration.validateFirstName(Firstname))
-                Console.WriteLine("True");
-            else
-                Console.WriteLine("False");
+
+
+
+
+
+
+
         }
+        
     }
 }
--- UserRegistration.cs
+++ UserRegistration.cs
@@ -1,19 +1,37 @@
-﻿using System.Text.RegularExpressions;
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UserRegistration_Day19
 {
-    class UserRegistration
+    public class UserRegistration
     {
         
 
-        public string regex_for_name = "^[A-Z]{1}[a-z]{2,10}$";
+        public string regex_for_fname = "^[A-Z]{1}[a-z]{2,10}$";
+        public string regex_for_lname = "^[A-Z]{1}[a-z]{2,10}$";
 
-        public bool validateFirstName(string fName)
+
+
+        public void validatefirstname(string fName)
         {
 
-            return Regex.IsMatch(fName, regex_for_name);
-
+            if (Regex.IsMatch(fName, regex_for_fname))
+                Console.WriteLine("First name is correct");
+            else
+                Console.WriteLine("First name is not correct");
+        }
+        public void validateLastName(string lName)
+        {
+            if (Regex.IsMatch(lName, regex_for_lname))
+                Console.WriteLine("Last name is correct");
+            else
+                Console.WriteLine("Last name is not correct");
         }
     }
 }
